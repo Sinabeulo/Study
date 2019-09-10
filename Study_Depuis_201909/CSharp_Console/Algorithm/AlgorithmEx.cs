c2c1137b5539837ae15f11e07c7eac950df95f64
MIM0520 <ITSCORE@LAPTOP-E7MAVBF6>
2019-09-10 14:11:39 +0900

    Algo

---
+++ Study_Depuis_201909/CSharp_Console/Algorithm/AlgorithmEx.cs
@@ -0,0 +1,42 @@
+﻿using CSharp_Console.GlobalInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Console.Algorithm
+{
+    class AlgorithmEx
+    {
+        public AlgorithmEx()
+        {
+            bool isExit = false;
+
+            IExampleRun example = null;
+
+            while (isExit == false)
+            {
+                Console.WriteLine("Algorithm Category");
+                Console.Write(" >> : ");
+                string cmd = Console.ReadLine();
+
+                switch (cmd.ToLower())
+                {
+                    case "1":
+                    case "pn":
+                        example = new PrimaryNumber();
+                        break;
+
+                    case "exit": case "quit": case "e": case "q": isExit = true; break;
+                }
+                
+                example?.RunExample();
+
+                Console.Write("Press Enter Key....");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Study_Depuis_201909/CSharp_Console/Control.cs
@@ -0,0 +1,68 @@
+﻿using CSharp_Console.Algorithm;
+using CSharp_Console.GlobalInterface;
+using DesignPattern.DecoratorPattern;
+using DesignPattern.ObserverPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Console
+{
+    class Control
+    {
+        public Control()
+        {
+            while (true)
+            {
+                Console.WriteLine("Input Category");
+                Console.Write(" >> : ");
+                string cmd = Console.ReadLine();
+                switch (cmd.ToLower())
+                {
+                    case "dp":
+                        DesignPatternsExample();
+                        break;
+
+                    case "al":
+                        AlgorithmEx algorithmEx = new AlgorithmEx();
+                        break;
+                }
+            }
+        }
+
+        private void DesignPatternsExample()
+        {
+            bool isExit = false;
+
+            IExampleRun example = null;
+
+            while (isExit == false)
+            {
+                Console.WriteLine("Input Design Pattern");
+                Console.Write(" >> : ");
+                string cmd = Console.ReadLine();
+
+                switch (cmd.ToLower())
+                {
+                    case "1":
+                    case "observer":
+                        example = new WeatherStation();
+                        break;
+                    case "2":
+                    case "deco":
+                        example = new Decorator();
+                        break;
+
+                    case "exit": case "quit": case "e": case "q": isExit = true; break;
+                }
+                example?.RunExample();
+
+                Console.Write("Press Enter Key....");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
