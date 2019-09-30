5d701f8882bd64ae36c3d6c0a02a241296957311
MIM0520 <ITSCORE@LAPTOP-E7MAVBF6>
2019-09-30 19:40:46 +0900

    Add WPF FileManager

---
+++ Study_Depuis_201909/CSharp_WPF/Controls/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace CSharp_WPF.Controls
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetProperty<T>(ref T prop, string propName, T value)
+        {
+            prop = value;
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Study_Depuis_201909/CSharp_WPF/Model/MainModel.cs
@@ -0,0 +1,28 @@
+﻿using CSharp_WPF.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CSharp_WPF.Model
+{
+    class MainModel : ViewModelBase
+    {
+        private UserControl _contentWindow;
+
+        public MainModel() { }
+
+        //internal void SetNewContent(UserControl _content)
+        //{
+        //    ContentWindow = _content;
+        //}
+
+        public UserControl ContentWindow
+        {
+            get => _contentWindow; 
+            set => SetProperty(ref _contentWindow, nameof(ContentWindow), value);
+        }
+    }
+}
