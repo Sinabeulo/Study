b9533f50500163ada21a41935f3ceffae9d1821f
MIM0520 <ITSCORE@LAPTOP-E7MAVBF6>
2019-09-30 17:27:57 +0900

    Add WPF Project

---
+++ Study_Depuis_201909/CSharp_WPF/Controls/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace CSharp_WPF.Controls
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetProperty<T>(ref T prop, string propName, T value)
+        {
+            prop = value;
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
---
+++ Study_Depuis_201909/CSharp_WPF/VIewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CSharp_WPF.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_WPF.VIewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
