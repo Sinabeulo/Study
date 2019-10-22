4649dd188cd868e1731c509e267b8225bdf7d43d
MIM0520 <ITSCORE@LAPTOP-E7MAVBF6>
2019-10-22 18:03:01 +0900

    JS_Update

--- JavaScript/FT_Sample.js
+++ JavaScript/FT_Sample.js
@@ -117,3 +117,85 @@ function f_parameter(){
     f(o);
     console.log(`f를 호출한 다음: o.message="${o.message}"`);
 }
+//=================================================================================
+//2019-10-22
+// 크라운 앤 앵커 게임 예제
+function f_crown_n_anchor(bets ){
+    // m 이상 n 이하의 무작위 정수를 반환합니다.
+    function rand(m,n){
+        return m + Math.floor((n - m + 1)*Math.random());
+    }
+
+    // 크라운 앤 앵커 게임의 여섯 그림 중 하나에 해당하는 문자열을 무작위로 반환합니다.
+    function randFace(){
+        return ["crown","anchor","heart","spade","club","diamond"][rand(0,5)];
+    }
+
+    let funds = 50;
+    let round = 0;
+
+    while (funds > 1 && funds < 100) {
+        round++;
+        console.log(`round ${round}`);
+        console.log(`\tstarting funds: ${funds}p`);
+        // 돈을 겁니다.
+        let bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0 };
+        let totalBet = rand(1, funds);
+        if (totalBet === 7) {
+            totalBet = funds;
+            bets.heart = totalBet;
+        } else {
+            // 나눕니다.
+            let remaining = totalBet;
+            do {
+                let bet = rand(1, remaining);
+                let face = randFace();
+                bets[face] = bets[face] + bet;
+                remaining = remaining - bet;
+            } while (remaining > 0)
+        }
+
+        funds = funds - totalBet;
+        console.log('\tbets: ' +
+            Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(',') +
+            `(total:${totalBet} pence)`);
+
+        // 주사위
+        const hand = [];
+        for (let roll = 0; roll < 3; roll++) {
+            hand.push(randFace());
+        }
+
+        console.log(`\thand: ${hand.join(',')}`);
+
+        let winnings = 0;
+        for (let die = 0; die < hand.length; die++) {
+            let face = hand[die];
+            if (bets[face] > 0) winnings = winnings + bets[face];
+        }
+        funds = funds + winnings;
+        console.log(`\twinnings: ${winnings}`);
+    }
+    console.log(`\tendgin funds: ${funds}`);
+}
+
+// 피보나치 예제
+function fibonachi(){
+    for(let tmep, i=0, j=1; j<30; temp = i, i = j, j = i + temp)
+        console.log(j);
+}
+
+// cancel side effect (부수효과 취소)
+// 부수 효과 = 외부의 상태를 변경하는 것 또는 함수로 들어온 인자의 상태를 직접 변경하는 것
+function cancelSidEffect(){
+    const skipIt = true;
+    let x = 0;
+    const result = skipIt || x++;   // true면 x => 0 (부수효과 미발생)
+    // skipIt이 true일 때 x++을 처리하지 않는 행위를 단축평가라고 한다.
+    console.log(result + ' : ' + x);
+
+    const doIt = false;
+    let y = 0;
+    const result1 = doIt && y++;
+    console.log(result1 + ' : ' + y);
+}
